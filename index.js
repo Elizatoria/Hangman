5e2a84d5188fe90af5d28998bec3678768937cbd
Elizatoria <Elizatoria@gmail.com>
2022-11-16 21:00:05 -0500

    Selecting Random Words

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import prompt from "readline-sync";
 import wordBank from "./wordbank.js";
 
-const randomnumber = math.round(math.random() * wordBank.length);
-console.log(randomnumber);
+const randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+console.log(randomWord);
